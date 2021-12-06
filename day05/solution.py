4de3c87c8270a612deec9f8b6bdc7ac8ee799615
Wessel Radstok <wradstok@gmail.com>
2021-12-06 13:08:31 +0000

    Cleanup day 5 slightly

--- day05/solution.py
+++ day05/solution.py
@@ -14,51 +14,45 @@ with open(os.getcwd() + "/day05/input.txt") as f:
         begin, end = map(lambda x: x.split(","), input_line)
         begin, end = map(int, begin), map(int, end)
         begin, end = Point(next(begin), next(begin)), Point(next(end), next(end))
-        # Swap points if in the wrong order.
-        if begin > end:
-            lines.append(Line(end, begin))
-        else:
-            lines.append(Line(begin, end))
+        lines.append(Line(begin, end))
 
 # Determine the size of the grid.
 ending = Point(max([line.end.x for line in lines]), max([line.end.y for line in lines]))
 
+
+def draw_lines(grid, lines: List[Line], diagonal: bool) -> None:
+    # Draw the lines.
+    for line in lines:
+        if line.start.x == line.end.x:
+            # Horizontal position equal, so vertical line.
+            dir = 1 if line.end.y > line.start.y else -1
+            for i in range(line.start.y, line.end.y + 1 * dir, dir):
+                grid[i][line.start.x] += 1
+        elif line.start.y == line.end.y:
+            # Vertical positions equal, so horizontal line.
+            dir = 1 if line.end.x > line.start.x else -1
+            for i in range(line.start.x, line.end.x + 1 * dir, dir):
+                grid[line.start.y][i] += 1
+        elif diagonal:
+            # Exactly 45 degrees, so it doesn't matter whether use x or y
+            line_length = abs(line.end.x - line.start.x)
+            x_dir = 1 if line.end.x > line.start.x else -1
+            y_dir = 1 if line.end.y > line.start.y else -1
+            for i in range(line_length + 1):
+                grid[line.start.y + i * y_dir][line.start.x + i * x_dir] += 1
+
+
+def get_overlapping(grid, min_hit: int) -> int:
+    return sum(map(lambda row: len([1 for hit in row if hit >= min_hit]), grid))
+
+
 # Part 1.
 grid: List[List[int]] = [[0 for _ in range(ending.x + 1)] for _ in range(ending.y + 1)]
-
-# Draw the lines.
-for line in lines:
-    if line.start.x == line.end.x:
-        # Horizontal position equal, so vertical line.
-        for i in range(line.start.y, line.end.y + 1):
-            grid[i][line.start.x] += 1
-    elif line.start.y == line.end.y:
-        # Vertical positions equal, so horizontal line.
-        for i in range(line.start.x, line.end.x + 1):
-            grid[line.start.y][i] += 1
-
-overlapping = sum(map(lambda row: len([1 for hit in row if hit >= 2]), grid))
-print(f"{overlapping} points have at least two overlapping lines.")
+draw_lines(grid, lines, False)
+print(f"{get_overlapping(grid, 2)} points have at least two overlapping lines.")
 
 
 # Part 2
 grid: List[List[int]] = [[0 for _ in range(ending.x + 1)] for _ in range(ending.y + 1)]
-for line in lines:
-    if line.start.x == line.end.x:
-        # Horizontal position equal, so vertical line.
-        for i in range(line.start.y, line.end.y + 1):
-            grid[i][line.start.x] += 1
-    elif line.start.y == line.end.y:
-        # Vertical positions equal, so horizontal line.
-        for i in range(line.start.x, line.end.x + 1):
-            grid[line.start.y][i] += 1
-    else:
-        # Exactly 45 degrees, so it doesn't matter whether use x or y
-        line_length = abs(line.end.x - line.start.x)
-        for i in range(line_length + 1):
-            x_dir = 1 if line.end.x > line.start.x else -1
-            y_dir = 1 if line.end.y > line.start.y else -1
-            grid[line.start.y + i * y_dir][line.start.x + i * x_dir] += 1
-
-overlapping = sum(map(lambda row: len([1 for hit in row if hit >= 2]), grid))
-print(f"{overlapping} points have at least two overlapping lines.")
+draw_lines(grid, lines, True)
+print(f"{get_overlapping(grid, 2)} points have at least two overlapping lines.")
